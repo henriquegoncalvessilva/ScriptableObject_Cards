59ba31b421a3175d519ae194618c5ce3a6190271
Henrique Gonçalves <luizhenriquegoncalvessilva@hotmail.com>
2019-12-04 10:17:48 -0300

    Add files via upload

---
+++ CardGame/Scripts/Card.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+[ExecuteInEditMode]
+[CreateAssetMenu(fileName = "Card", menuName = "CardGame/Generate Card", order = 1)]
+public class Card : ScriptableObject
+{
+    
+    //Image of Card
+    public Sprite imageCard;
+
+
+    //Info of Card
+    public string nameCard;
+
+    public string descriptionCard;
+
+    public types typeCard;
+
+    public int attackCard;
+
+    public int defenseCard;
+
+    public enum types
+    {
+        //If you want add more type of card in ENUMERATOR, so follow the structure below: ↓
+        SpeelCaster,
+        Dragon,
+        Beast,
+        Effect,
+        Warrior,
+        Plant
+
+    }
+
+     
+
+
+   
+
+
+}
---
+++ CardGame/Scripts/CardManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CardManager : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    Card card;
+
+    void Awake()
+    {
+        ChangeInfo();
+
+    }
+
+    void ChangeInfo()
+    {
+
+        //Get all info of ScriptableObject and implement in Prefab. 
+
+        transform.GetChild(1).transform.GetChild(0).GetComponent<Text>().text = card.nameCard.ToUpper();
+        transform.GetChild(1).transform.GetChild(1).GetComponent<Text>().text = "[ " + card.typeCard + " ]";
+        transform.GetChild(1).transform.GetChild(2).GetComponent<Text>().text = card.descriptionCard;
+        transform.GetChild(1).transform.GetChild(3).GetComponent<Text>().text = "ATK/ " + card.attackCard.ToString();
+        transform.GetChild(1).transform.GetChild(4).GetComponent<Text>().text = "DEF/ " + card.defenseCard.ToString();
+
+
+        transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = card.imageCard;
+
+
+    }
+}
